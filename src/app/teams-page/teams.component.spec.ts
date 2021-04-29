966dac89ced38f117ebe5b5bab840de6c172d84b
azat <azatmagdeev@gmail.com>
2021-04-29 08:36:49 +0300

    github action added

--- src/app/teams-page/teams.component.spec.ts
+++ src/app/teams-page/teams.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TeamsComponent } from './teams.component';
+import { TeamsPageComponent } from './teams.component';
 
 describe('TeamsComponent', () => {
-  let component: TeamsComponent;
-  let fixture: ComponentFixture<TeamsComponent>;
+  let component: TeamsPageComponent;
+  let fixture: ComponentFixture<TeamsPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TeamsComponent ]
+      declarations: [ TeamsPageComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TeamsComponent);
+    fixture = TestBed.createComponent(TeamsPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
